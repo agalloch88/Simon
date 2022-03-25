82aea1bda618497c8db5c271a3b2cd74aabcafbe
Ryan Kirsch <agalloch88@gmail.com>
2022-03-25 07:05:02 -0400

    add jquery animation and play sound

--- game.js
+++ game.js
@@ -3,8 +3,12 @@ gamePattern = [];
 buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  // select button with same id as randomChosenColor
+  $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
