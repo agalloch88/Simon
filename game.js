296ba30d2b86cd58301ade51249b8a1336ed5fda
Ryan Kirsch <agalloch88@gmail.com>
2022-04-03 20:19:39 -0400

    create checkAnswer and startOver functions for game

--- game.js
+++ game.js
@@ -21,10 +21,11 @@ $(".btn").click(function () {
   console.log(userClickedPattern);
   playSound(userChosenColor);
   animatePress(userChosenColor);
-  // checkAnswer(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
@@ -46,4 +47,30 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $(`#${currentColor}`).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
